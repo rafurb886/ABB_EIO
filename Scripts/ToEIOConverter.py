21784d3b07cdd23ef6d645672b77c2ed66dcd9ec
rafal <rafalurbanczykv@gmail.com>
2022-06-16 11:55:42 +0200

    tests of cells (part2)

--- Scripts/ToEIOConverter.py
+++ Scripts/ToEIOConverter.py
@@ -1,8 +1,9 @@
 import regex as re
 import pandas as pd
 import numpy as np
-import itertools
 from helper import check_uniqe_val_in_column, drop_column_if_exist, add_column_if_no_exist, df_remove_other_column
+
+import itertools
 
 class DataRequirementsToConvertSignals:
     columns_name = ['Name', 'SignalType', 'Device', 'Label', 'DeviceMap', 'Category', 'Access', 'Default', 'SafeLevel',
@@ -17,16 +18,21 @@ class DataRequirementsToConvertSignals:
                                  'EncType': 'NAN',      'MaxLog': np.NaN,      'MaxPhys': np.NaN,
                                  'MaxPhysLimit': np.NaN,'MaxBitVal': np.NaN,    'MinLog': np.NaN,
                                  'MinPhys': np.NaN,     'MinPhysLimit': np.NaN, 'MinBitVal': np.NaN}
-
-    available_signal_type = ['AI', 'AO', 'DI', 'DO', 'GI', 'GO']
-    available_signal_safe_level = ['SAFETYSAFELEVEL', 'NAN']
-    available_signal_access = ['READONLY', 'NAN']
-    available_signal_enctype = ['UNSIGNED', 'SIGNED', 'NAN']
+    available_null_name =  {'Name': False,          'SignalType': False,    'Device': False,
+                            'Label': True,          'DeviceMap': False,     'Category': True,
+                            'Access': True,         'Default': True,        'SafeLevel': True,
+                            'EncType': True,        'MaxLog': True,         'MaxPhys': True,
+                            'MaxPhysLimit': True,   'MaxBitVal': True,      'MinLog': True,
+                            'MinPhys': True,        'MinPhysLimit': True,   'MinBitVal': True}
+    available_signals_param = { 'SignalType':   ['AI', 'AO', 'DI', 'DO', 'GI', 'GO'],
+                                'SafeLevel':    ['SAFETYSAFELEVEL'], # usuniete NANy
+                                'Access':       ['READONLY'],
+                                'EncType':      ['UNSIGNED', 'SIGNED']}
 
     regex_for_mapping = r'(\d{1,4})(-\d{1,4})?'
     regex_for_label = r'[\w _]+'
     regex_for_names = r'[_a-zA-Z0-9]+'
-    regex_for_user_names = {'Name': regex_for_names,        'Device': regex_for_names,    'Label': regex_for_label,
+    regex_for_user_names = {'Name': regex_for_names,        'Device': regex_for_names,      'Label': regex_for_label,
                             'DeviceMap': regex_for_mapping, 'Category': regex_for_names}
 
 
@@ -36,24 +42,63 @@ class ValidateSignalsCellsInLine(DataRequirementsToConvertSignals):
         self.line = line
 
     def check_all_cells_valid(self):
-        self.line = self.check_correct_character_in_columns(['Name', 'DeviceMap'])
+        self.line = self.check_correct_character_in_columns(self.regex_for_user_names.keys())
+        self.line = self.check_correct_parameters_in_columns(self.available_signals_param.keys())
         return self.line
 
-    def check_correct_character(self, string_to_check, regex_str=r'[_a-zA-Z0-9]+', available_null=False):
-        if not available_null:  # dodac nulla
-            pattern = re.compile(regex_str)
-            return pattern.fullmatch(string_to_check)
+    @staticmethod
+    def check_correct_character(string_to_check, regex_str=r'[_a-zA-Z0-9]+', available_null=False, default_null_value=np.NaN):
+        if available_null and string_to_check == default_null_value:
+            return True
+        pattern = re.compile(regex_str)
+        return pattern.fullmatch(string_to_check)
 
     def check_correct_character_in_columns(self, columns_name):
         for column in columns_name:
-            if self.check_correct_character(self.line[column], regex_str=self.regex_for_user_names[column]):
+            if self.check_correct_character(self.line[column],
+                                            regex_str=self.regex_for_user_names[column],
+                                            available_null=self.available_null_name[column],
+                                            default_null_value=self.default_value_for_columns[column]):
+                continue
+            self.line[column] = input(f'Wrong {column}: {self.line[column]} in signal {self.line["Name"]}.'
+                                      f'\nEnter correct {column}: ')
+            self.check_correct_character_in_columns([column])
+        return self.line
+
+    def check_correct_parameters_in_columns(self, columns_name):
+        for column in columns_name:
+            if self.line[column] in self.available_signals_param[column] \
+                    or (self.available_null_name[column] and self.line[column] in self.available_signals_param[column]):
+                continue
+            self.line[column] = input(f'Wrong {column}: {self.line[column]} in signal {self.line["Name"]}. \n'
+                                      f'Enter correct {column}: ')
+            self.check_correct_parameters_in_columns([column])
+        return self.line
+
+    def check_is_digit(self, column_name):
+        for column in column_name:
+            if isinstance(self.line[column], float) or isinstance(self.line[column], int):
+                continue
+            self.line[column] = input(f'{column} in signal {self.line["Name"]} is not a digit. '
+                                      f'Current value: {self.line[column]}\n Enter correct value: ')
+            self.check_is_digit([column])
+        self.line[column_name] = self.line[column_name].astype('float')
+        return self.line
+
+    def check_default_column(self, column='Default'):
+        if isinstance(self.line[column], float) or isinstance(self.line[column], int):
+            if self.line['SignalType'] in ['DI', 'DO'] and self.line[column] in [0, 1] \
+                    or self.line['SignalType'] in ['GI', 'GO'] and self.line[column] % 1 == 0\
+                    or self.line['SignalType'] in ['AI', 'AO'] and isinstance(self.line[column], float)\
+                    or pd.isna(self.line[column]):
                 return self.line
-            self.line[column] = input(f'Wrong {column}: {self.line[column]}. \nEnter correct {column}: ')
-            return self.check_correct_character_in_columns([column])
+
+        self.line[column] = input(f'Default value in signal {self.line["Name"]} is not valid. Current value: {self.line["Default"]} in signal type {self.line["SignalType"]} \n Enter correct value: ')
+        self.check_default_column()
+        return self.line
 
 
-
-class SignalsConverter(ValidateSignalsCellsInLine):
+class SignalsConverter(DataRequirementsToConvertSignals): # nie wiem czy dodać dziedziczenie z ValidateSignalsCellsInLine
 
     def __init__(self, data):
         '''
@@ -61,7 +106,7 @@ class SignalsConverter(ValidateSignalsCellsInLine):
             data: pandas object containing all data to convert
         '''
         self.data = data
-
+        super().__init__(None)
 
     @classmethod
     def from_excel(cls, path):
