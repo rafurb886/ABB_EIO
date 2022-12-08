d11ce236340137f4b6d7bb82b19ae254f6c79e72
rafal <rafalurbanczykv@gmail.com>
2022-12-08 22:28:33 +0100

    additional_function - override all, override signals, append signals works. To additional test

--- ABB_EIO_translation/scripts/app_qt_window_file_exist.py
+++ ABB_EIO_translation/scripts/app_qt_window_file_exist.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QLabel, \
                             QDialog,\
                             QPushButton, \
                             QVBoxLayout, \
-                            QRadioButton,\
+                            QRadioButton
 from PyQt5.QtCore import Qt
 from .app_qt_styles import *
 
